2ac47fe53a03074b6589a641922a5f9ec4fe9702
jjh1232 <dlwjdwns424@gmail.com>
2025-02-27 21:35:16 +0900

    translate

--- src/customhook/Admintools/AdminDeclesdata.jsx
+++ src/customhook/Admintools/AdminDeclesdata.jsx
@@ -103,6 +103,28 @@ const {noticeid,isdecles}=props;
     if(error){
         return <>에러입니다!: {error.message}</>
     }
+
+    //이유 단어 문자열 각각변경
+    const translatereason=(reason)=>{
+        console.log("단어"+reason)
+        const dictionary={
+            spam:"스팸및광고",
+            discomfort:"불쾌감",
+            violent:"폭력적",
+            nsfw:"선정적",
+            baduser:"잘못된유저",
+            etc:"기타"
+        }
+        return dictionary[reason.toLowerCase()]||reason;
+    }
+    const Translated=({text})=>{
+        const transarray=text.replace(/[\[\]]/g,'') //정규식으로대괄호제거
+                            .split(',')  //스플릿으로단어나누기
+                            .map(word=>word.trim()) //공백제거해야 뒤에것도 인식함
+                            .map(translatereason); //반복하며함수실행
+                
+                            return <>{transarray.join(`,`)}</>//구분을위해 다시 ,추가
+    }
     return (
         <Modalout>
             <Exitbutton onClick={()=>{isdecles(false)}}/>
@@ -125,7 +147,7 @@ const {noticeid,isdecles}=props;
                     {decle.username}
                     </DecleList>
                     <DecleList style={{width:"200px"}}>
-                    {decle.reason}
+                    <Translated text={decle.reason} />
                     </DecleList>
                     <DecleList style={{width:"130px"}}>
                     {decle.datetime}
