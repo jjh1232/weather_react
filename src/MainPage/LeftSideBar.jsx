e16a85f190a8aec5a93762c0868dea218c514d0a
jjh1232 <dlwjdwns424@gmail.com>
2025-03-26 17:59:05 +0900

    newweatherdataver2

--- src/MainPage/LeftSideBar.jsx
+++ src/MainPage/LeftSideBar.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import Userweather from "../UI/Userweather";
+import Userweather2 from "../UI/Noticetools/Userweather2";
 
 const Wrapper=styled.div`
 width:15%;
@@ -19,8 +20,9 @@ function LeftSideBar(){
     if(window.location.pathname.includes("/admin")) return null
     return (
         <Wrapper>
-            <Userweather/>
-            
+            {//<Userweather/>
+            }
+            <Userweather2/>
         </Wrapper>
     )
 }
