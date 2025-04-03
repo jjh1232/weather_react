f167a3ffbe33525cdf72a16b65be32296a826f5e
jjh1232 <dlwjdwns424@gmail.com>
2025-04-03 23:41:24 +0900

    backgroundver0.2

--- src/MainPage/MainLayout.jsx
+++ src/MainPage/MainLayout.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import Header from "./Header";
 import AdminLeft from "../admin/AdminLeft";
@@ -19,6 +19,34 @@ const Wrapper=styled.div`
     
     
 `
+//날짜에따른테마
+const thema={
+    dawn: {
+        sky: ['#87CEEB', '#64BFFF'],
+        horizon: '#90EE90',
+        ground: ['#8B4513', '#A0522D']
+      },
+      morning: {
+        sky: ['#FF4500', '#FFD700'],
+        horizon: '#FF6347',
+        ground: ['#8B0000', '#A52A2A']
+      },
+        noon: {
+        sky: ['#FF4500', '#FFD700'],
+        horizon: '#FF6347',
+        ground: ['#8B0000', '#A52A2A']
+      },
+      evening: {
+        sky: ['#FF4500', '#FFD700'],
+        horizon: '#FF6347',
+        ground: ['#8B0000', '#A52A2A']
+      },
+      night: {
+        sky: ['#FF4500', '#FFD700'],
+        horizon: '#FF6347',
+        ground: ['#8B0000', '#A52A2A']
+      }
+}
 // 1. 기본 구조 선택
 const Background = styled.div`
   position: fixed;
@@ -26,22 +54,23 @@ const Background = styled.div`
   height: 100vh;
   z-index: -10px;
   //overflow: hidden;
-`;
+ `
 
 // 2. 하늘 레이어
 const Sky = styled.div`
   height: 90%;
-  background: linear-gradient(
-    180deg,
-    rgba(135,206,235,1) 0%,
-    rgba(0,191,255,1) 100%
+  //그라데이션 아마 시간대별로 조정
+  background: linear-gradient( 
+    0deg,
+    ${({ thema }) => thema.sky[0]} 0%,
+    ${({ thema }) => thema.sky[1]} 100%
   );
 `;
 
 // 3. 지평선 효과
 const Horizon = styled.div`
   height: 10px;
-  background: rgba(144,238,144,0.8);
+  background: ${({ $thema}) => $thema[0]};
   box-shadow: 0 0 10px rgba(144,238,144,0.5);
 `;
 
@@ -51,12 +80,14 @@ const Ground = styled.div`
   background: 
     linear-gradient(
       0deg,
-      rgba(139,69,19,1) 0%,
-      rgba(160,82,45,1) 100%
+      ${({ thema }) => thema.sky[0]} 0%,
+      ${({ thema }) => thema.sky[1]} 100%
     );
 `;
+
+
 export default function MainLayout(props){
-    const {children}=props
+    const [thema,setThema]=useState();
 
     return (
         <Wrapper>
