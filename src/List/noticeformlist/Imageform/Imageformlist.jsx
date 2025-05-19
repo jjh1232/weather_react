38d919a47c83ddc9df043544c31b4d0f8818221a
jjh1232 <dlwjdwns424@gmail.com>
2025-05-19 23:48:29 +0900

    imagelistcss

--- src/List/noticeformlist/Imageform/Imageformlist.jsx
+++ src/List/noticeformlist/Imageform/Imageformlist.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import styled from "styled-components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCopy as imagesicon } from "@fortawesome/free-regular-svg-icons";
+import { faHeart as heart } from "@fortawesome/free-regular-svg-icons";
 
 const Wrapper=styled.div`
+
     border: 1px solid red;
     width: 270px;
     margin-right:1px ;
     height: 300px;
-    overflow: hidden;
+    //overflow: hidden;
 `
 const Header=styled.div`
    border : 1px solid blue ;
@@ -31,13 +35,25 @@ const Titlediv=styled.div`
 const Main=styled.div`
     width: 100%;
     height: 100%;
-    
+    position: relative;
     border: 1px solid blue;
 `
 const MainImage=styled.img`
     width: 100%;
-    height: 100%;
+    height: 80%;
     object-fit: cover;
+`
+const Imagenumdiv=styled.div`
+position: absolute;
+    border: 1px solid red;
+    top: 1px;
+    z-index: 10;
+`
+const Likebuttondiv=styled.div`
+position: absolute;
+     border: 1px solid red;
+     z-index: 10px;
+     bottom: 10px;
 `
 
 export default function Imageformlist(props){
@@ -60,9 +76,16 @@ export default function Imageformlist(props){
        
         </Userandtitlediv>
     
+        
             </Header>
         <Main>
             <MainImage src={process.env.PUBLIC_URL+content.mainimage}/>
+            <Imagenumdiv>
+                <FontAwesomeIcon icon={imagesicon}/>
+            </Imagenumdiv>
+            <Likebuttondiv>
+                <FontAwesomeIcon icon={heart}/>
+            </Likebuttondiv>
         </Main>
       
 
