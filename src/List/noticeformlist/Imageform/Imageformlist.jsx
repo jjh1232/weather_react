107ba4931b47a3fd394e13574ab59d35fab9e74d
jjh1232 <dlwjdwns424@gmail.com>
2025-05-22 23:53:21 +0900

    csshard

--- src/List/noticeformlist/Imageform/Imageformlist.jsx
+++ src/List/noticeformlist/Imageform/Imageformlist.jsx
@@ -16,9 +16,11 @@ const Wrapper=styled.div`
     overflow: hidden;
 `
 const Header=styled.div`
-   border : 1px solid blue ;
+   //border : 1px solid blue ;
    display: flex;
+   width: 100%;
    height: 50px;
+      min-width: 0;
 `
 const Profileimg=styled.img`
     width: 40px;
@@ -27,25 +29,47 @@ const Profileimg=styled.img`
     border: 1px solid black;
 `
 const Userandtitlediv=styled.div`
+    width: 230px;
     display: flex;
     flex-direction: column;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
 `
 const Userdatadiv=styled.div`
-    border-bottom: 1px solid black ;
+    width: 100%;
+    display: flex;
+    border-bottom: 1px solid blue ;
 `
+const Nicknamediv=styled.div`
+    font-size: 15px;
+    margin-right: 10px;
+`
+const Usernamediv=styled.div`
+    font-size: 10px;
+    color: gray;
+`
+
 const Titlediv=styled.div`
-    
+border: 1px solid red;
+     width: 185px;
+    display: flex;
+    flex-direction: column;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    min-width: 0;
 `
 const Main=styled.div`
     width: 100%;
-    height: 100%;
+    height: 250px;
     position: relative;
     border: 1px solid blue;
 `
 const MainImage=styled.img`
     width: 100%;
-    height: 80%;
-    object-fit: cover;
+    height: 100%;
+    object-fit: fill;
 `
 const Imagenumdiv=styled.div`
 position: absolute;
@@ -62,17 +86,32 @@ position: absolute;
 const Likebuttondiv=styled.div`
 position: absolute;
        
-     bottom: 70px;
+     bottom: 10px;
      right: 5px;
    
   color: white; 
 `
-
-
+/*
+const Likenumdiv=styled.div`
+   
+ border-radius: 4px;
+    //padding: 1px 5px;
+    font-size: 13px;
+    background-color: rgba(255, 255, 255, 0.4); 
+  color: black; 
+    right: 8px;
+    bottom: 5px;
+    text-align: center;
+    position: absolute;
+    text-align: center;
+    width: 40px;
+`
+*/
 export default function Imageformlist(props){
     const {content}=props;
     let logincheck=AuthCheck();
    let axiosinstance=CreateAxios()
+   
     const Noticelikehandler=()=>{
         if(logincheck){
              
@@ -93,12 +132,15 @@ export default function Imageformlist(props){
                 <Profileimg src={process.env.PUBLIC_URL+"/userprofileimg"+content.userprofile}/>
         <Userandtitlediv>
             <Userdatadiv>
+                <Nicknamediv>
                  {content.nickname}
-                {content.username}
+                </Nicknamediv>
+                
+                
        
             </Userdatadiv>
            <Titlediv>
-             {content.title}
+             {content.title}zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz
             </Titlediv> 
        
         </Userandtitlediv>
@@ -112,10 +154,12 @@ export default function Imageformlist(props){
                 <FontAwesomeIcon icon={imagesicon} /> {content.imagenum}
             </Imagenumdiv>
             <Likebuttondiv onClick={()=>{Noticelikehandler()}}>
-                <FontAwesomeIcon icon={heart} size="xl" color={content.likely?"red":"black"} style={{position:"absolute",right:"1px",bottom:"1px"}}/>
-                <FontAwesomeIcon icon={fullheart} size="xl" color={content.likely?"red":"white"}  style={{position:"absolute",right:"1px",bottom:"1px"}}/>
 
+                <FontAwesomeIcon icon={fullheart} size="xl" color={content.likely?"red":"white"}  style={{position:"absolute",right:"1px",bottom:"1px"}}/>
+                <FontAwesomeIcon icon={heart} size="xl" color={content.likely?"red":"black"} style={{position:"absolute",right:"1px",bottom:"1px"}}/>
+                        
             </Likebuttondiv>
+             
         </Main>
       
 
