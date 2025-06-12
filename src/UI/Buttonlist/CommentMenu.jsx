db9c26e04f2f1732dbf87e306807b27eaefca9e8
jjh1232 <dlwjdwns424@gmail.com>
2025-06-12 23:58:58 +0900

    menuandlogic

---
+++ src/UI/Buttonlist/CommentMenu.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { useCookies } from "react-cookie";
+import styled from "styled-components";
+import AuthCheck from "../../customhook/authCheck";
+
+const Wrapper=styled.div`
+    background-color: gray;
+    position: absolute;
+    width: 150px;
+    right: 5px;
+    border: 1px solid black;
+    box-shadow: 0 4px 12px rgba(0, 0, 0, 0.9);  // 부드러운 그림자
+  border-radius: 8px;        
+  
+    &:hover {
+    box-shadow: 0 6px 20px rgba(0, 0, 0, 0.25); // 호버 시 그림자 강조
+  }
+`
+
+export default function CommentMenu(props){
+    const {nickname,ismenu}=props;
+    const [cookie,setcookie,removecookie]=useCookies(["userinfo"])
+
+    const logincheck=AuthCheck();
+
+
+
+    return (
+        <Wrapper>
+            <div>
+삭제
+            </div>
+            
+                <div>
+팔로우
+            </div>
+                <div>
+유저차단
+            </div>
+
+        </Wrapper>
+    )
+}
