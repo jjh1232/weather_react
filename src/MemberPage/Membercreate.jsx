cf01828e484a8ee8574b3931dc6c6304fe39fce6
jjh1232 <dlwjdwns424@gmail.com>
2025-07-24 22:19:15 +0900

    createformcss

--- src/MemberPage/Membercreate.jsx
+++ src/MemberPage/Membercreate.jsx
@@ -7,8 +7,14 @@ import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 
 import { Validators } from "../UI/Modals/Validators";
-import ParseJwt from "../customhook/ParseJwt";
-import { useCookies } from "react-cookie";
+//폰트어섬
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser as usericon } from "@fortawesome/free-regular-svg-icons";
+import { faUnlockKeyhole as passwordicon } from "@fortawesome/free-solid-svg-icons";
+import { faClipboardCheck as confirmicon } from "@fortawesome/free-solid-svg-icons";
+import { faIdCard as profileicon } from "@fortawesome/free-regular-svg-icons";
+import { faHouse as regionicon } from "@fortawesome/free-solid-svg-icons";
+
 const Wrapper=styled.div`
 position: relative;
 
@@ -22,28 +28,98 @@ flex-direction: column;
 `
 const Headerdiv=styled.div`
     position: relative;
-    margin-top:50px;
-    border: 1px solid red;
+    margin-top:30px;
+    border: 1px solid gray;
     width: 600px;
+    height: 80px;
+    margin-bottom:30px;
 `
 const Maindiv=styled.div`
+    display: flex;
     position: relative;
     
     width: 600px;
-    border: 1px solid blue;
+   align-items: center;
+   justify-content: center;
+
 `
-const Formrow=styled.tr`
+const StyledForm=styled.form`
+    width: 100%;
+`
+const StyledTable=styled.table`
+    width: 100%;
+`
+const Formrow=styled.div`
+    min-height: 50px;
+    display: flex;
+    width: 100%;
+    flex-direction:column;
+  
+    
+`
+
+
+const Inputcell=styled.div`
+    display: flex;
+    text-align: left;
+    gap: 6px;
+      background-color: #fff;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  padding: 8px 12px;
+
+`
+const SubButtondiv=styled.div`
+    
+    margin-left: auto;
+`
+const SubButton=styled.button`
+    
+`
+//아이콘
+const StyledIcon=styled(FontAwesomeIcon)`
+    border: 1px solid blue;
+    margin-top: 4px;
+    color:black;
+`
+const Inputarea=styled.input`
+    background-color: #fff;
+    border: none;
+    outline: none;
+   max-width: 400px;
+    height: 22px;
+
+    //pladceholder스타일
+    &::placeholder{
+        
+        color:#999;
+        font-style: italic; 
+         font-size: 14px; 
+        opacity: 1;   
+    }
+`
+const Errordiv=styled.div`
+display: flex;
+    text-align: center;
+    align-items: center;
+    height: 30px;
+     font-size:15px;
+    color: red;
+    
+`
+const Footerdiv=styled.div`
+margin-top: 30px;
+ border: 1px solid blue;
+ display:flex;
+ justify-content: center;
+
+`
+const SubmitButton=styled.button`
+    width: 400px;
     height: 50px;
 `
-const Labelcell=styled.td`
-    width: 120px;
-    text-align: right;
-    padding-right: 10px;
-    font-weight: bold;
-`
-const Inputcell=styled.td`
-    text-align: left;
-`
+
+
 function membercreate(){
  
     const [form,setform]=useState(
@@ -114,7 +190,7 @@ function membercreate(){
         
         axios.get("/open/emailcheck",{
             params:{
-            username:loginform.username
+            username:form.username
             }
         }).then((res)=>
         {   console.log(res.data.check)
@@ -139,7 +215,7 @@ function membercreate(){
         e.preventDefault();
         axios.get("/open/profileidcheck",{
             params:{
-                profileid:loginform.profileid
+                profileid:form.profileid
             }
         }).then((res)=>{
             if(res.data){
@@ -214,107 +290,138 @@ return(
         헤더
     </Headerdiv>
     <Maindiv>
-   <form onSubmit={handleSubmit}>
-        <table>
+   <StyledForm onSubmit={handleSubmit}>
+        <StyledTable>
           <tbody>
             <Formrow>
-              <Labelcell>
-                이메일:
-                </Labelcell>
+            
                 <Inputcell>
-              
-                <input
+                <StyledIcon icon={usericon}/>
+                <Inputarea
                   name="username"
                   value={form.username}
                   onChange={handleChange}
                   type="text"
+                  placeholder="로그인이메일"
                 />
-                <Button title="중복검사" onClick={checkEmail} />
+                <SubButtondiv>
+                     <SubButton onClick={checkEmail} >중복검사</SubButton>
+                </SubButtondiv>
+               
+                </Inputcell>
+                <Errordiv>
+
+              
                 {errors.username && <p>{errors.username}</p>}
-             </Inputcell>
+                  </Errordiv>
+             
+
             </Formrow>
 
             <Formrow>
-              <Labelcell>
-                비밀번호:
-                 </Labelcell>
+             
                  <Inputcell>
-                <input
+                 <StyledIcon icon={passwordicon}/>
+                <Inputarea
                   name="password"
                   value={form.password}
                   onChange={handleChange}
                   type="password"
+                  placeholder="비밀번호"
                 />
-                {errors.password && <p>{errors.password}</p>}
-             </Inputcell>
-            </Formrow>
-
-            <Formrow>
-              <Labelcell>
-                비밀번호 확인:
-                 </Labelcell>
-                <Inputcell>
-                <input
-                  name="confirmpassword"
-                  value={form.confirmpassword}
-                  type="password"
-                  onChange={handleChange}
-                />
-                {!form.confirmpassword ||
-                form.password === form.confirmpassword ? null : (
-                  <p>비밀번호가 일치하지 않습니다</p>
-                )}
                 </Inputcell>
+                  <Errordiv>
+                {errors.password && <p>{errors.password}</p>}
+                    </Errordiv>
              
             </Formrow>
 
             <Formrow>
-              <Labelcell>
-                프로필ID:
-                  </Labelcell>
+              
+                <Inputcell>
+                <StyledIcon icon={confirmicon}/>
+                <Inputarea
+                  name="confirmpassword"
+                  value={form.confirmpassword}
+                  type="password"
+                  onChange={handleChange}
+                  placeholder="비밀번호확인"
+                />
+                </Inputcell>
+                    <Errordiv>
+                {!form.confirmpassword ||
+                form.password === form.confirmpassword ? null : (
+                  <p>비밀번호가 일치하지 않습니다</p>
+                )}
+                    </Errordiv>
+               
+             
+            </Formrow>
+
+            <Formrow>
+             
                  <Inputcell>
-                <input
+                 <StyledIcon icon={profileicon}/>
+                <Inputarea
                   name="profileid"
                   value={form.profileid}
                   onChange={handleChange}
+                  placeholder="프로필id"
                 />
-                <Button title="중복검사" onClick={checkProfileId} />
-                {errors.profileid && <p>{errors.profileid}</p>}
+                  <SubButtondiv>
+                               <SubButton onClick={checkProfileId} >중복검사</SubButton>
+                </SubButtondiv>
+     
                 </Inputcell>
+                    <Errordiv>
+                {errors.profileid && <p>{errors.profileid}</p>}
+                </Errordiv>
+               
             
             </Formrow>
 
             <Formrow>
-              <Labelcell>
-                닉네임:
-                </Labelcell>
+             
                 <Inputcell>
-                <input
+                <StyledIcon icon={usericon}/>
+                <Inputarea
                   name="nickname"
                   value={form.nickname}
                   onChange={handleChange}
+                  placeholder="닉네임"
                 />
+                </Inputcell>
+                <Errordiv>
+
+                
                 {errors.nickname && <p>{errors.nickname}</p>}
-              </Inputcell>
+                  </Errordiv>
+            
             </Formrow>
 
             <Formrow>
-              <Labelcell>
-                지역:
-                 </Labelcell>
+             
                  <Inputcell>
-                <input name="region" value={form.region} readOnly />
-                <Weatherregion title="지역 찾기" onGetdata={handleRegionSelect} />
+                 <StyledIcon icon={regionicon}/>
+                <Inputarea name="region" value={form.region} readOnly placeholder="지역" />
+
+                <SubButtondiv>
+                     <Weatherregion title="지역 찾기" onGetdata={handleRegionSelect} />
+                </SubButtondiv>
+               
              </Inputcell>
             </Formrow>
           </tbody>
-        </table>
+        </StyledTable>
+        <Footerdiv>
 
-        <Button title="회원가입" type="submit" />
-      </form>
+       
+        <SubmitButton  type="submit" >회원가입</SubmitButton>
+         </Footerdiv>
+      </StyledForm>
                 
     </Maindiv>
-
+    
     
  
     </Wrapper>
